e126a0e685a2c600a2071d7e30b9da1f5cb06c7b
Garoe Gonzalez <garoegonzalez@gmail.com>
2015-08-03 20:21:22 +0200

    Cleaning and first plot of coffee analysis

--- OfficeCoffee/OfficeCoffee.R
+++ OfficeCoffee/OfficeCoffee.R
@@ -1,5 +1,7 @@
 library("xlsx")
-
+library("lubridate")
+library("ggplot2")
+library(scales)
 setwd("/Users/Garo/Desktop/GitHub/main_repository/OfficeCoffee")
 
 data_raw<-read.xlsx("Kawa-rozliczenia.xlsx",sheetIndex=1)
@@ -7,14 +9,37 @@ data_raw<-read.xlsx("Kawa-rozliczenia.xlsx",sheetIndex=1)
 # We clean few
 data_clean<-data_raw[,-c(2,87,88)]
 # We invert the data frame
-data<-t(data_clean)
+data_clean<-t(data_clean)
 # We obtain the coffees per interval
-data<-data[c(1,seq(2,nrow(data),by=3)),]
-rownames(data)<-1:nrow(data)
+data_clean<-data_clean[c(1,seq(2,nrow(data_clean),by=3)),]
+data_clean[1,20]<-"Total"
+#rownames(data)<-1:nrow(data)
 
-for (i in seq(2,nrow(data),by=3)){
-  date<-as.character(data[i,1])
-  print(date)
-  #df = rbind(df, data.frame(date=date, y, z))
-}
+values<-data_clean[2:29,2:20]
+mode(values)<-'numeric'
+data<-data.frame(values)
+#data_final$date<-data[2:29,1]
+rownames(data)<-1:nrow(data)
+#colnames(data)<-data[1,2:20]
+# We add the names
+Names<-c("JKM","KHU","KLI","KBA","KPY","KSZ","LDO","LST","MKL","MZA","MBR","MNA","MWA","PBL","WBO","GGP","PKR","KMO","Total")
+colnames(data)<-Names
+# We add the timestamps and properly parser it as Date variable
+data$date<-data_clean[2:29,1]
+data$date<-as.Date(data$date,format="%d.%m.%Y")
+# We set all not registered numbers to 0
+data[is.na(data)]<-0
+
+
+ggplot(data, aes(x=as.POSIXct(date),y=Total)) +
+  geom_smooth(ce=FALSE, fill=NA, size=2) +
+  geom_point()+
+  scale_x_datetime(breaks = date_breaks("2 months"))+
+  ylab("Absolute number of coffee's drunk")+
+  xlab("Date")+
+  ggtitle("Coffee's taken at MSR/DAR Gdanks office between Jul 2014 and Jul 2015")+
+  theme(plot.title=element_text(size=18))+
+  theme_bw(base_family="Times")
+#ggsave("Measured_predicted_RF.pdf")
+
 
