7dd5bd99ace587cb5bcfbcfed87c28f1263c8c9f
Garoe Gonzalez <garoegonzalez@gmail.com>
2015-08-20 19:44:24 +0200

    Project finish

--- Coursera_Getting_and_Cleaning_Data/Project/run_analysis.R
+++ Coursera_Getting_and_Cleaning_Data/Project/run_analysis.R
@@ -1,4 +1,3 @@
-
 ## We check that we have the data in our working directory
 filesToOpen<-c("UCI HAR Dataset/features.txt",
                "UCI HAR Dataset/train/X_train.txt",
@@ -40,5 +39,26 @@ dataTest<-cbind(dataSubTest,dataYTest,dataXTest)
 ## We merge Train and Test data sets
 data<-rbind(dataTrain,dataTest)
 
-##To be continued
-grepl("*mean*",names(data))
+## We obtain the column numbers that contain the patterns
+## mean, Mean and std and we combine them in a vector
+## with "SubjectID"    "ActivityType" in positions 1 and 2
+dataWithmean<-which(grepl("*mean*",names(data)))
+dataWithMean<-which(grepl("*Mean*",names(data)))
+dataWithstd<-which(grepl("*std*",names(data)))
+
+ColumnsToUse<-c(1,2,dataWithmean,dataWithMean,dataWithstd)
+
+## We get only the columns in which we are interested
+data<-data[,ColumnsToUse]
+
+## We change the Activity variable to a factor with proper label
+Activitylabels=c("WALKING","WALKING_UPSTAIRS","WALKING_DOWNSTAIRS","SITTING","STANDING","LAYING")
+data$ActivityType<-factor(data$ActivityType,levels=c(1,2,3,4,5,6),labels = Activitylabels)
+
+################### Our data is clean and ready to be used
+## Data frame ->   data
+
+################# We compute the mean for every subject and activity
+##### We end up with a dataframe of 180 rows (30 subjects* 6 activities)
+### and 88 columns corresponding to all variables with mean and std
+tidydata<-aggregate( . ~ SubjectID+ActivityType, data, mean)
